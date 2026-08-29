6c85a832fb92fee0bbca188c62aa2745c45b800a
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-29 20:39:05 +0530

    Time: 0 ms (100%), Space: 14.1 MB (90.39%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
-    vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
+// set_intersection STL Function
+// TC - O(NlogN + MlogM)
+// SC - O(1)
+vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
 {
     sort(nums1.begin(), nums1.end());
     sort(nums2.begin(), nums2.end());
-
     vector<int> ans;
 
-    int i = 0;
-    int j = 0;
+    // Requires sorted arrays for intersection
+    set_intersection(
+        nums1.begin(), nums1.end(),
+        nums2.begin(), nums2.end(),
 
-    while (i < nums1.size() && j < nums2.size())
-    {
-        if (nums1[i] == nums2[j])
-        {
-            int val = nums1[i];
-            ans.push_back(val);
-            i++;
-            j++;
-        }
-        else if (nums1[i] > nums2[j])
-        {
-            ++j;
-        }
-        else
-        {
-            ++i;
-        }
-    }
+        // Using back_inserter to append to an empty vector
+        back_inserter(ans));
+
     return ans;
 }
 };
