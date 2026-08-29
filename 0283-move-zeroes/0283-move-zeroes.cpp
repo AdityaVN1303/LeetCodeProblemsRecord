94fa21a5e347bdd1496fac9c97fec84aa8607cf1
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-29 19:56:41 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.8%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,8 +4,8 @@ public:
         int i = -1;
         for(int j = 0; j<nums.size(); ++j){
             if(nums[j] != 0){
-                nums[++i] = nums[j];
-                if(i != j ) nums[j] = 0;
+                swap(nums[++i] , nums[j]);
+                
             }
         }
     }
