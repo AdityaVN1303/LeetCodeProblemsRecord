decac66f9c5504097269f592315365b4093cdee1
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-30 16:30:26 +0530

    Time: 0 ms (100%), Space: 11.2 MB (83.1%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int n = nums.size();
-        int pivot = -1;
-        for(int i=0; i<n-1; ++i){
-            if(nums[i] > nums[i+1]){
-                pivot = i+1;
-            }
+        int consecutive = 0;
+        int itr = 0;
+        int i = 0;
+
+        if(nums.size() == 1) return true;
+        while(itr < 2*n){
+            int next = (i+1)%n;
+            if(nums[i] <= nums[next]) consecutive++;
+            else consecutive = 0;
+
+            if(consecutive == n-1) return true;
+            i = next;
+            itr++;
         }
-
-        if(pivot == -1) return true;
-
-        int itr = 1;
-        while(itr < n){
-            int next = (pivot+1)%n;
-            if(nums[pivot] > nums[next]) return false;
-            pivot = next;
-            ++itr;
-        }
-
-        return true;
+        return false; 
     }
 };
