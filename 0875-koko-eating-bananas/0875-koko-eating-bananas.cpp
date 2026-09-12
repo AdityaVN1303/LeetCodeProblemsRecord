84ba3f0b7a4dc7d3ab078acec7ed110e2ff4a8ce
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-09-12 08:45:38 +0530

    Time: 8 ms (54.17%), Space: 23 MB (24.3%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -13,8 +13,6 @@ public:
         int i=1;
         int j = *max_element(piles.begin() , piles.end());
 
-        int ans = 0;
-
         while(i <= j){
 
             int mid = i + (j-i)/2;
@@ -22,13 +20,12 @@ public:
             long long val = calcHours(piles , mid);
 
             if(val <= h){
-                ans = mid;
                 j = mid-1;
             }
             else{
                 i = mid+1;
             }
         }
-        return ans;
+        return i;
     }
 };
